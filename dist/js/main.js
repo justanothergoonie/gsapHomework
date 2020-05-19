8db7c03bf92e6d6407bd71016cc91adc9280f57f
Cody J Powell <codyj.powell@outlook.com>
2020-05-19 14:39:55 -0400

    added gsap animation and svg logos

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,2 @@
-"use strict";
-
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+"use strict";console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files."),gsap.to(".logo",{duration:2,x:300,y:300,backgroundColor:"purple",borderRadius:"20%",border:"5px solid white",ease:"bounce",width:"25%"});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 console.log(`Hello World from main.js! 
 Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+to verify that you're working in the right files.`);
+
+gsap.to('.logo', {
+	duration: 2,
+	x: 300,
+	y: 300,
+	backgroundColor: 'purple',
+	borderRadius: '20%',
+	border: '5px solid white',
+	ease: 'bounce',
+	width: '25%',
+});
